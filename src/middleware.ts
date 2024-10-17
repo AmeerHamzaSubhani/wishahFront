6ba1e6571beff559e00549f8547d179bf8166bc1
Ameer Hamza Subhani <77618368+ah155209@users.noreply.github.com>
2024-10-16 20:30:45 -0700

    changes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,10 +7,8 @@ export function middleware(request: NextRequest) {
     // Public paths that don't require authentication
     const publicPaths = [
         '/',
-        '/auth/login',
-        '/auth/signup',
-        '/ourcourses',
-        '/about',
+        '/login',
+        '/signup',
     ];
 
     // Admin paths
@@ -18,47 +16,31 @@ export function middleware(request: NextRequest) {
     const userPaths = ['/dashboard'];
 
     // Get user info from cookie
-    const token = request.cookies.get('access-token')?.value;
+    const token = request.cookies.get('access_token')?.value;
     const isAdmin = request.cookies.get('is-admin')?.value === 'true';
 
-    const isAuthenticated = !!token;
-
     // Case 1: Authenticated users trying to access public routes
-    if (publicPaths.includes(pathname) && isAuthenticated) {
+    if (publicPaths.includes(pathname) && token) {
         return NextResponse.redirect(new URL('/', request.nextUrl));
     }
 
     // Case 2: Unauthenticated users trying to access private routes
-    if (!publicPaths.includes(pathname) && !isAuthenticated) {
+    if (!publicPaths.includes(pathname) && !token) {
         return NextResponse.redirect(new URL('/', request.nextUrl));
     }
 
     // Case 3: Non-admin users trying to access admin routes
     if (adminPaths.some(path => pathname.startsWith(path)) && !isAdmin) {
         return NextResponse.redirect(
-            new URL('/dashboard/my-courses', request.nextUrl),
+            new URL('/dashboard', request.nextUrl),
         );
     }
-    // Case 4: Admin users trying to access user routes
-    if (userPaths.some(path => pathname.startsWith(path)) && !!isAdmin) {
-        return NextResponse.redirect(
-            new URL('/admin/dashboard', request.nextUrl),
-        );
-    }
-    // Allow the request to proceed for valid cases
     return NextResponse.next();
 }
 
-// Config for matching the paths that will trigger this middleware
 export const config = {
     matcher: [
         '/',
-        '/profile',
-        '/auth/login',
-        '/auth/signup',
-        '/ourcourses',
-        '/about',
-        '/admin/:path*',
-        '/dashboard/:path*',
+        '/:path*',
     ],
 };
